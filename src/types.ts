0cc5e3d29acc45aa8aca1ad52a03cede4abbbaee
Rostislav Kremecek <rostislav.kremecek@gmail.com>
2020-09-09 05:46:36 +0200

    project restructuring to frontend and backend

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-import { EntityManager, IDatabaseDriver, Connection } from '@mikro-orm/core';
-
-export type MyContext = {
-  req: Request,
-  em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>,
-  res: Response
-}
