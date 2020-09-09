7f1c170687b64820a6d79a8a4b0d126df3231d21
Rostislav Křemeček <rostislav.kremecek@ictmedia.cz>
2020-09-09 11:57:15 +0200

    fix issue with Input props

--- web/src/components/InputField.tsx
+++ web/src/components/InputField.tsx
@@ -4,18 +4,18 @@ import {useField} from "formik";
 
 type InputFieldProps  =  React.InputHTMLAttributes<HTMLInputElement> & {
   label: string,
-  placeholder?: string,
+  // placeholder?: string,
   name: string,
 };
 
-const InputField: React.FC<InputFieldProps> = (props) => {
+const InputField: React.FC<InputFieldProps> = ({label, size: _, ...props}) => {
 
   const [field, {error}] = useField(props);
 
   return (
     <FormControl isInvalid={!!error} style={{ margin: '1rem 0' }}>
-      <FormLabel htmlFor="name">{props.label}</FormLabel>
-      <Input {...field} id={field.name} placeholder={props.placeholder} />
+      <FormLabel htmlFor="name">{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} placeholder={props.placeholder} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
